f2a9080340f965fb63d2cffe7bf736a47281beb6
EsmaNErdem <esmaerdem94@gmail.com>
2024-04-14 17:27:51 -0400

    creating aws CI/CD pipeling

---
+++ scripts/application_start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Give appropriate permissions to the express-app directory
+sudo chown -R ec2-user:ec2-user /home/ec2-user/express-app
+sudo chmod -R 755 /home/ec2-user/express-app
+
+# Navigate to the working directory
+cd /home/ec2-user/express-app
+
+# Set up NVM environment
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads NVM
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads NVM bash_completion (node is in path now)
+
+# Install Node.js modules
+npm install
+
+# Start the Node.js application
+node server.js > server.out.log 2> server.err.log < /dev/null &
